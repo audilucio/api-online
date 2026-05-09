d203485b25c24c34c46f4ceb5bf2c77968d76eab
Audilucio <audilucio.fisico@gmail.com>
2026-05-09 03:45:59 -0300

    projeto inicial

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Servidor rodando na porta ${port}`);
+}
+
+bootstrap();
