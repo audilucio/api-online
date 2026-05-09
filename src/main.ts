5442afea8a822fe00d1e2b53434a64a5aef82df2
Audilucio <audilucio.fisico@gmail.com>
2026-05-09 05:04:20 -0300

    adicionando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Monitoramento')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
 
   const port = process.env.PORT || 3000;
 
   await app.listen(port, '0.0.0.0');
 
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor online na porta ${port}`);
 }
 
 bootstrap();
